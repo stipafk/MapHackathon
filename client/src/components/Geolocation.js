42247e9d85f071e1bb3e6273628c5442413459e9
lv <linkevich_va@astralnalog.ru>
2018-12-01 10:41:50 +0300

    покрасил иконку геолокации

--- client/src/components/Geolocation.js
+++ client/src/components/Geolocation.js
@@ -35,7 +35,7 @@ class Geolocation extends Component {
               var extent = that.props.positionFeature.getGeometry().getExtent();
               that.props.map
                 .getView()
-                .fit(extent, { maxZoom: 18, duration: 600 });
+                .fit(extent, { maxZoom: 16, duration: 600 });
               that.setState({ isZoom: true, status: true });
             }
           });
@@ -48,7 +48,7 @@ class Geolocation extends Component {
             var extent = that.props.positionFeature.getGeometry().getExtent();
             that.props.map
               .getView()
-              .fit(extent, { maxZoom: 18, duration: 600 });
+              .fit(extent, { maxZoom: 16, duration: 600 });
             that.setState({ isZoom: true, status: true });
           }
         }
@@ -78,7 +78,7 @@ class Geolocation extends Component {
             fill-rule="evenodd"
             clip-rule="evenodd"
             d="M41 49C41 42.9249 45.9249 38 52 38C58.0751 38 63 42.9249 63 49C63 55.0751 58.0751 60 52 60C45.9249 60 41 55.0751 41 49ZM53 57.9451V55C53 54.4477 52.5523 54 52 54C51.4477 54 51 54.4477 51 55V57.9451C46.8284 57.4839 43.5161 54.1716 43.0549 50H46C46.5523 50 47 49.5523 47 49C47 48.4477 46.5523 48 46 48H43.0549C43.5161 43.8284 46.8284 40.5161 51 40.0549V43C51 43.5523 51.4477 44 52 44C52.5523 44 53 43.5523 53 43V40.0549C57.1716 40.5161 60.4839 43.8284 60.9451 48H58C57.4477 48 57 48.4477 57 49C57 49.5523 57.4477 50 58 50H60.9451C60.4839 54.1716 57.1716 57.4839 53 57.9451Z"
-            fill="#121ADA"
+            fill={this.state.status ? "#121ADA" : "gray"}
           />
           <defs>
             <filter
@@ -142,7 +142,7 @@ const ContainerGeolocation = styled.div`
   position: absolute;
   right: 30px;
   top: 6px;
-  opacity: 0.9;
+  opacity: 0.8;
   &:hover {
     opacity: 1;
   }
